cc91b1f568970449fa6aefb1af8e0e5a9189a239
Ky Marie <clanphere15@gmail.com>
2024-07-31 11:07:26 +0000

    new file logic

---
+++ src/context.js
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(!!
+        localStorage.getItem('token'));
+        const history = useHistory();
+
+        const login = async (username, password) => {
+            try {
+                const response = await axios.post('/api/auth/login', { username, password });
+                localStorage.setItem('token', response.data.token);
+                setIsAuthentiacted(true);
+                history.push('/account');
+            } catch (error) {
+                console.error('Login Failed, Please try again or click 'Forgot Password' button below!', error);
+            }
+        };
+
+        const logout = () => {
+            localStorage.removeItem('token');
+            setIsAuthenticated(false);
+            history.push('/login');
+        };
+
+        return (
+            <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+                {children}
+                </AuthContext.Provider>
+        );
+};
+
+export const useAuth = () => useContext(AuthContext);
