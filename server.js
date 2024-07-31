bd0b962455f5fe056edf01cc4e92fa907df36425
Ky Marie <clanphere15@gmail.com>
2024-07-31 08:46:38 +0000

    fixed errors in server.js

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const bodyParser = require('body-parser');
 const db = require('./models');
 const authRoutes = require('./routes/authRoutes');
 const accountRoutes = require('./routes/accountRoutes');
-const transactionRoutes = require('./routes/transactionRoutes';
+const transactionRoutes = require('./routes/transactionRoutes');
 
 
-)
 const app = express()
 app.use(bodyParser.json());
 
@@ -16,20 +15,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/account', accountRoutes);
 app.use('/api/transactions', transactionRoutes);
 
-app.get('/', function (req, res) {
-    res.render('index', {app: null, error: null});
-})
-
-app.post('/', function(req, res) {
-    let url = 'http://api.skrillaxchange.com/data/2.5/app?q=${input}&units=metric&appid=${apiKey}'
-    console.log(req.body.)
-});
-
 
 const PORT = process.env.PORT || 3000;
 
 db.sequelize.sync().then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
-    });
+    })
 });
